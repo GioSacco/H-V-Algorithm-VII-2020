68b5f2ac8cb96fc5bea8a757bb262daf7231caa8
Giovanni Sacco <giovanni.sacco@eng.it>
2020-08-13 13:12:28 +0200

    Evoluzione sul disegno dei nodi

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -16,14 +16,15 @@ var root = null;
 // VARIABILI PER H-V DRAWING
 //
 var leftHvDrawingTree = [];
-var rightHvDrawingTree = [];
-var dummyDepth = 1;
+var leftHvDrawingTreeMap = d3.map();
 
-var HvNode = function(id, left, right, depth, x, y) {
+var rightHvDrawingTree = [];
+var rightHvDrawingTreeMap = d3.map();
+
+var HvNode = function(id, left, right, x, y) {
   this.id = id;
   this.left = left;
   this.right = right;
-  this.depth = depth;
   this.x = x;
   this.y = y;
 };
@@ -45,45 +46,102 @@ function postOrder (node) {
 
 }
 
-// CALCOLA LA PROFONDITA' DI OGNI NODO
-//
-function drawTree(nodes, type){
+function updateNodes(nodeID){
 
-  if(nodes.length > 0){
-    var node = nodes.shift();
+  var node = leftHvDrawingTreeMap.get(nodeID);
 
-    if(!node.left && !node.right){
-      var newNode = new HvNode(node.id, node.left, node.right, dummyDepth, 0, 0);
+  if(node.left && !node.right){
 
-      type == 'LEFT' && leftHvDrawingTree.push(newNode);
-      type == 'RIGHT' && rightHvDrawingTree.push(newNode);
+    var childNode = node.left;
+    leftHvDrawingTreeMap.set(childNode.id, new HvNode(childNode.id, childNode.left, childNode.right, node.x, node.y+100));
 
-      drawTree(nodes, type);
+    updateNodes(childNode.id);
 
-    }else{
-      dummyDepth++;
-      var newNode = new HvNode(node.id, node.left, node.right, dummyDepth, 0, 0);
-      
-      type == 'LEFT' && leftHvDrawingTree.push(newNode);
-      type == 'RIGHT' && rightHvDrawingTree.push(newNode);
+  }
 
-      drawTree(nodes, type);
+  if(!node.left && node.right){
 
-    }
+    var childNode = node.right;
+    leftHvDrawingTreeMap.set(childNode.id, new HvNode(childNode.id, childNode.left, childNode.right, node.x, node.y+100));
+
+    updateNodes(childNode.id);
+
+  }
+
+  if(node.left && node.right){
+
+    var childRightNode = node.right;
+    leftHvDrawingTreeMap.set(childRightNode.id, new HvNode(childRightNode.id, childRightNode.left, childRightNode.right, node.x+100, node.y));
+    updateNodes(childRightNode.id);
+
+    var childLeftNode = node.left;
+    leftHvDrawingTreeMap.set(childLeftNode.id, new HvNode(childLeftNode.id, childLeftNode.left, childLeftNode.right, node.x, node.y+100));
+    updateNodes(childLeftNode.id);
 
   }
 
 }
 
-function resizeDepth(hvNodes){
-  var maxValue = d3.max(hvNodes, function(d) { return +d.depth});
+function drawTree(subTree){
 
-  hvNodes.forEach(element => {
-    element.depth = maxValue - element.depth +1;
-    element.y = element.depth*20;
+  subTree.forEach(element => {
+    updateNodes(element.id);
+  })
+
+}
+
+function drawTreeMap(subTreeList){
+
+  var dummySubTreeMap = d3.map();
+
+  subTreeList.forEach(newNode => {
+    dummySubTreeMap.set(newNode.id, new HvNode(newNode.id, newNode.left, newNode.right, 0, 0));
   });
 
-  return hvNodes;
+  return dummySubTreeMap;
+
+}
+
+
+// MAIN FUNCTION
+//
+function _initFunction(){
+
+  // LEGGI IL FILE JSON DA UN FILE LOCALE
+  //
+  d3.json("./assets/nodes.json").then( (data) => {
+      inputTree = data;
+
+      // RICAVO L'ALBERO IN POST ORDINE
+      //
+      postOrder(inputTree[0]);
+
+      // PRENDO LA RADICE
+      //
+      var dummyRoot = postOrderTree.pop();
+      root = new HvNode(dummyRoot.id, dummyRoot.left, dummyRoot.right, 0, 0);
+
+      // SUDDIVIDO TRA SOTTOALBERO DESTRO E SINISTRO
+      //
+      var dummyLeftSubTree = preOrderTree.slice(0, preOrderTree.indexOf(dummyRoot));
+      var dummyRightSubTree = preOrderTree.slice(preOrderTree.indexOf(dummyRoot)+1);
+
+      leftSubTree = postOrderTree.filter(value => dummyLeftSubTree.includes(value));
+      rightSubTree = postOrderTree.filter(value => dummyRightSubTree.includes(value));
+      
+      // CREA DUE MAP DI SUPPORTO PER L'AGGIORNAMENTO DELLE
+      // COORDINATE DEI VARI NODI
+      //
+      leftHvDrawingTreeMap = drawTreeMap(leftSubTree);
+      drawTree(leftSubTree);
+      console.log(leftHvDrawingTreeMap);
+
+      updateDrawing(leftHvDrawingTreeMap.values());
+
+      //rightHvDrawingTreeMap = drawTreeMap(rightSubTree);
+      //console.log(rightHvDrawingTreeMap);
+                           
+  });
 
 }
 
@@ -91,33 +149,6 @@ function resizeDepth(hvNodes){
 * @param data 
 */
 function updateDrawing(data){
-
- var body = d3.select("body")
-   .attr("width", width)
-   .attr("height", height)
- 
- svg = body.append("svg")
-   .attr("width", width)
-   .attr("height", height)
-   .append("g")  
-
- var nodes = svg.selectAll(".node").data(data, function(d){return d.id});
-
- // Exit clause: Remove elements
- nodes.exit().remove();
-
- // Enter clause: add new elements
- //
- nodes.enter().append("rect")
-     .attr("class", "node")
-     .attr("x", function(d) { return 10; })
-     .attr("y", function(d) { return d.y })
-     .attr("width", 10)
-     .attr("height", 10);
-
-}
-
-function updateDrawing2(data){
 
   var body = d3.select("body")
     .attr("width", width)
@@ -137,56 +168,12 @@ function updateDrawing2(data){
   //
   nodes.enter().append("rect")
       .attr("class", "node")
-      .attr("x", function(d) { return 100; })
+      .attr("x", function(d) { return d.x })
       .attr("y", function(d) { return d.y })
       .attr("width", 10)
       .attr("height", 10);
  
  }
-
-// MAIN FUNCTION
-//
-function _initFunction(){
-
-  // LEGGI IL FILE JSON DA UN FILE LOCALE
-  //
-  d3.json("./assets/nodes.json").then( (data) => {
-      inputTree = data;
-
-      // RICAVO L'ALBERO IN POST ORDINE
-      //
-      postOrder(inputTree[0]);
-
-      // PRENDO LA RADICE
-      //
-      var dummyRoot = postOrderTree.pop();
-      root = new HvNode(dummyRoot.id, dummyRoot.left, dummyRoot.right, 0, 0, 0);
-
-      // SUDDIVIDO TRA SOTTOALBERO DESTRO E SINISTRO
-      //
-      var dummyLeftSubTree = preOrderTree.slice(0, preOrderTree.indexOf(dummyRoot));
-      var dummyRightSubTree = preOrderTree.slice(preOrderTree.indexOf(dummyRoot)+1);
-
-      leftSubTree = postOrderTree.filter(value => dummyLeftSubTree.includes(value));
-      rightSubTree = postOrderTree.filter(value => dummyRightSubTree.includes(value));
-      
-      // CALCOLO LA PROFONDITA' DI OGNI NODO
-      //
-      dummyDepth = 1;
-      drawTree(leftSubTree, "LEFT");
-      leftHvDrawingTree = resizeDepth(leftHvDrawingTree);
-      console.log(leftHvDrawingTree);
-
-      updateDrawing(leftHvDrawingTree);
-
-      dummyDepth = 1;
-      drawTree(rightSubTree,  "RIGHT");
-      rightHvDrawingTree = resizeDepth(rightHvDrawingTree);
-      console.log(rightHvDrawingTree);
-                           
-  });
-
-}
 
 this._initFunction();
 
